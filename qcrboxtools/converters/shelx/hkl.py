e6dfc8c46645a6df64df976d4b337a336c21a182
Paul Niklas Ruth <panikruth@gmail.com>
2023-10-08 12:40:12 +0100

    implemented test for cif2hkl

--- qcrboxtools/converters/shelx/hkl.py
+++ qcrboxtools/converters/shelx/hkl.py
@@ -1,7 +1,16 @@
+import re
+import iotbx
 
+test_dev = """   7  -4  -5 2038.13 563.062   3
+   7  -5   0 13213.3 1168.96   3
+   7  -5  -1 2415.45 563.194   3
+"""
 
-def cif2hkl(cif_name, hkl_name, hkl_format):
-    pass
+def cif2hkl(cif_path, hkl_path, hkl_format):
+
+    # is shelx cif
+    with open(hkl_path, 'w', encoding='ASCII') as fo:
+        fo.write(test_dev)
 
 def hkl2cif(hkl_name, hkl_format, cif_name):
     pass
--- tests/converters/shelx/test_hkl.py
+++ tests/converters/shelx/test_hkl.py
@@ -1,12 +1,69 @@
 import pytest
 import numpy as np
+import re
+from qcrboxtools.converters.shelx.hkl import cif2hkl
 
-def test_cif_2_shelx_hkl(cif_name):
+
+
+def valid_hkl_line(line):
+    return re.match(r'[^\d\w\.\-]', line) is not None and len(line.strip()) > 0
+
+def read_hkl_line(line):
+    return [int(line[0:4]), int(line[4:8]),int(line[8:12]), float(line[12:20]), float(line[20:28]), int(line[28:])]
+
+def read_hkl_as_np(hkl_path, sort=False):
+    with open(hkl_path) as fo:
+        hkl_lines = [read_hkl_line(line) for line in fo.readlines() if valid_hkl_line(line)]
+    pivot = list(zip(*hkl_lines))
+    if len(pivot) == 5:
+        h, k, l, i, esd_i = pivot
+        number = None
+    elif len(pivot) == 6:
+        h, k, l, i, esd_i, number = pivot
+        number = np.array([int(val) for val in number])
+    else:
+        raise ValueError(f'len(pivot) was {len(pivot)} {str(pivot)}')
+    hkl = np.stack((h, k, l), axis=-1)
+    i = np.array(i)
+    esd_i = np.array(esd_i)
+    remove_zero_mask = np.logical_not(np.all(hkl == 0, axis=-1))
+    hkl = hkl[remove_zero_mask, :].copy()
+    i = i[remove_zero_mask].copy()
+    esd_i = esd_i[remove_zero_mask].copy()
+    if number is not None:
+        number = number[remove_zero_mask].copy()
+
+    if sort:
+        sort_mask = np.argsort(hkl[:,0] * 1e8 + hkl[:,1] * 1e4 + hkl[:,2])
+        hkl = hkl[sort_mask, :].copy()
+        i = i[sort_mask].copy()
+        esd_i = esd_i[sort_mask].copy()
+        if number is not None:
+            number = number[sort_mask].copy()
+    return hkl, i, esd_i, number
+
+
+@pytest.mark.parametrize('cif_path', [
+    './tests/converters/shelx/olex.cif',
+    './tests/converters/shelx/shelx.cif'
+])
+def test_cif_2_shelx_hkl(cif_path, tmp_path):
 
     # read shelxl hkl (created by olex)
+    target_path = 'tests/converters/shelx/target.hkl'
     # convert into numpy arrays hkl, intensity, esd
     # sort arrays by h, k, l
+    hkl_ref, i_ref, esd_ref, number_ref = read_hkl_as_np(target_path, True)
     # create converted hkl from cif into temporary file
+    out_hkl_path = tmp_path / "test.hkl"
     # read file the same way
+    cif2hkl(cif_path, out_hkl_path, 4)
+    hkl_test, i_test, esd_test, number_test = read_hkl_as_np(out_hkl_path, True)
     # compare whether identical
-    pass
+    assert hkl_ref.shape[0] == hkl_test.shape[0], "Not the same number of reflections"
+
+    assert np.all(hkl_ref == hkl_test), "miller indicees not the same"
+    assert np.all(np.abs(i_ref - i_test) < 0.01), "intensities not the same"
+    assert np.all(np.abs(esd_ref - esd_test) < 0.01), "esds not the same"
+
+

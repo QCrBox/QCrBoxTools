f6223264193d61d9cae75c8d6bfe2fded90c6ca6
Paul Niklas Ruth <panikruth@gmail.com>
2023-11-06 10:17:44 +0000

    Add docstrings

--- qcrboxtools/util/cif.py
+++ qcrboxtools/util/cif.py
@@ -1,10 +1,29 @@
+"""
+This module provides utility functions for handling CIF (Crystallographic Information File) files.
+It includes functionalities such as:
+- Safely reading CIF files.
+- Retrieving CIF datasets based on an index or identifier.
+- Extracting numerical values and estimated standard deviations (esds) from formatted CIF strings.
+- Replacing the structure block of a CIF file with another.
+"""
+
 from pathlib import Path
-from iotbx import cif
-from typing import Iterable
 import re
+from typing import Dict, Union, Tuple, Any, Iterable
+
+from iotbx import cif
 import numpy as np
 
-def read_cif_safe(cif_path):
+def read_cif_safe(cif_path: Union[str, Path]) -> Dict[str, Any]:
+    """
+    Reads the content of a CIF file with a Path from pathlib.
+
+    Args:
+    - cif_path (Union[str, Path]): The path to the CIF file.
+
+    Returns:
+    - Dict[str, Any]: The CIF model parsed from the file.
+    """
     with open(cif_path, 'r', encoding='UTF-8') as fobj:
         cif_content = fobj.read()
 
@@ -26,9 +45,20 @@ def cifdata_str_or_index(model: dict, dataset: [int, str]) -> cif.model.block:
         dataset = keys[dataset]
     return model[dataset]
 
-def split_esd_single(input_string: str):
-    PATTERN = r'([^\.]+)\.?(.*?)\(([\d\.]+)\)'
-    match = re.match(PATTERN, input_string)
+def split_esd_single(input_string: str) -> Tuple[float, float]:
+    """
+    Extracts the value and estimated standard deviation (esd) from a formatted string.
+
+    Args:
+    - input_string (str): The input string, expected to contain a numeric value and possibly an esd.
+
+    Returns:
+    - Tuple[float, float]: The value and its associated esd.
+    """
+    if not is_num_esd(input_string):
+        raise ValueError(f'{input_string} is not a valid string to split into value(esd)')
+    esd_pattern = r'([^\.]+)\.?(.*?)\(([\d\.]+)\)'
+    match = re.match(esd_pattern, input_string)
     if match is None:
         return float(input_string), np.nan
     if len(match.group(2)) == 0:
@@ -38,44 +68,78 @@ def split_esd_single(input_string: str):
         sign = -1
     else:
         sign = 1
-    # append the strings to reduce floating point errors
+    # append the strings to reduce floating point errors (do not use magnitude)
     value = float(match.group(1)) + sign * float('0.' + match.group(2))
     esd = magnitude * float(match.group(3).replace('.', ''))
     return value, esd
 
-def split_esds(input_strings: Iterable):
+def split_esds(input_strings: Iterable[str]) -> Tuple[np.ndarray, np.ndarray]:
+    """
+    Extracts values and esds from a list of formatted strings.
+
+    Args:
+    - input_strings (Iterable[str]): A list of input strings, each expected to
+      contain a numeric value and possibly an esd.
+
+    Returns:
+    - Tuple[np.ndarray, np.ndarray]: The values and their associated esds in
+      two separate arrays.
+    """
     values, esds = zip(*map(split_esd_single, input_strings))
     return np.array(list(values)), np.array(list(esds))
 
-def del_atom_site_condition(key):
+def del_atom_site_condition(key:str) -> bool:
+    """Check if to be deleted because an atom_site entry (these are replaced)"""
     return key.startswith('_atom_site')
 
-def del_geom_condition(key):
+def del_geom_condition(key:str) -> bool:
+    """Check if to be deleted because an geom entry (these are based on outdate info)"""
     return key.startswith('_geom')
 
-def del_refine_condition(key):
+def del_refine_condition(key:str) -> bool:
+    """Check if to be deleted because an refine entry (these combined cif is not converged)"""
     exceptions = ('_refine_ls_weighting','_refine_ls_extinction')
     return key.startswith('_refine') and not key.startswith(exceptions)
 
-def del_refln_condition(key):
+def del_refln_condition(key:str) -> bool:
+    """Check if to be deleted because a refln entry with calc (these are based on outdated info)"""
     return key.startswith('_refln') and '_calc' in key
 
-def del_refine_file(key):
+def del_refine_file(key:str) -> bool:
+    """Check if to be deleted because if containes the res/ins file (is replaced)"""
     test_keys = (
         '_shelx_res_file', # shelxl
         '_iucr_refine_instructions_details' # olex2
     )
     return any(key == test_key for test_key in test_keys)
 
-def is_num_esd(string):
+def is_num_esd(string: str) -> bool:
+    """
+    Check if character incompatible with numerical value with esd
+    is present.
+    """
     return re.search(r'[^\d\.\-\+\(\)]', string) is None
 
 def replace_structure_from_cif(
-    cif_path: Path,
+    cif_path: Union[str, Path],
     cif_dataset: str,
-    structure_cif_path: Path,
+    structure_cif_path: Union[str, Path],
     structure_cif_dataset: str,
-    output_cif_path: Path):
+    output_cif_path: Union[str, Path]
+) -> None:
+    """
+    Replaces the structural information of one dataset of one CIF file with the structural
+    information of a dataset of another CIF file.
+
+    Args:
+    - cif_path (Union[str, Path]): Path to the original CIF file.
+    - cif_dataset (str): Dataset/block name in the original CIF to replace.
+    - structure_cif_path (Union[str, Path]): Path to the CIF file from which the structure block
+      will be copied.
+    - structure_cif_dataset (str): Dataset/block name in the CIF file from which the structure
+      block will be copied.
+    - output_cif_path (Union[str, Path]): Path where the resulting CIF will be saved.
+    """
     cif_obj = read_cif_safe(cif_path=cif_path)
     structure_cif_obj = read_cif_safe(cif_path=structure_cif_path)
 
@@ -95,12 +159,12 @@ def replace_structure_from_cif(
     keys = tuple(new_block.loops.keys())
     for key in keys:
         if any(condition(key) for condition in conditions):
-            del(new_block[key])
+            del new_block[key]
 
     #delete items
     for key in new_block.keys():
         if any(condition(key) for condition in conditions):
-            del(new_block[key])
+            del new_block[key]
 
     structure_cif_block = structure_cif_obj[structure_cif_dataset]
     copy_loops = ('_atom_site', '_atom_site_aniso')
@@ -135,5 +199,3 @@ def replace_structure_from_cif(
 
     with open(output_cif_path, 'w', encoding='UTF-8') as fobj:
         fobj.write(str(new_cif_obj))
-
-
--- tests/util/test_cif.py
+++ tests/util/test_cif.py
@@ -1,10 +1,24 @@
+"""
+Test module for `qcrboxtools.util.cif`.
+
+This module contains unit tests for the various utility functions related to
+crystallographic information file (CIF) manipulation found in `qcrboxtools.util.cif`.
+It tests functionality like extracting estimated standard deviations (ESDs) from
+formatted strings and manipulating CIF data structure.
+"""
+
 from pathlib import Path
+from typing import Tuple
 
-from pytest import approx
-import pytest
 import numpy as np
+import pytest
 
-from qcrboxtools.util.cif import split_esds, split_esd_single, replace_structure_from_cif, read_cif_safe
+from qcrboxtools.util.cif import (
+    read_cif_safe,
+    replace_structure_from_cif,
+    split_esd_single,
+    split_esds
+)
 
 @pytest.mark.parametrize('string, test_value, test_esd', [
     ('100(1)', 100.0, 1),
@@ -14,15 +28,17 @@ from qcrboxtools.util.cif import split_esds, split_esd_single, replace_structure
     ('2.1(1.3)', 2.1, 1.3),
     ('0.648461', 0.648461, np.nan)
 ])
-def test_split_esd_single(string, test_value, test_esd):
+def test_split_esd_single(string: str, test_value: float, test_esd: float):
+    """Test the `split_esd_single` function with various formatted strings."""
     val, esd = split_esd_single(string)
-    assert val == approx(test_value)
+    assert val == pytest.approx(test_value)
     if np.isnan(esd) or np.isnan(test_esd):
         assert np.isnan(esd) and np.isnan(test_esd)
     else:
-        assert esd == approx(test_esd)
+        assert esd == pytest.approx(test_esd)
 
 def test_split_esds():
+    """Test the `split_esds` function."""
     strings = [
         '0.03527(13)',
         '0.02546(10)',
@@ -44,12 +60,20 @@ def test_split_esds():
     values, esds = split_esds(strings)
 
     for value, esd, (check_value, check_esd) in zip(values, esds, solutions):
-        assert value == approx(check_value)
-        assert esd == approx(check_esd)
+        assert value == pytest.approx(check_value)
+        assert esd == pytest.approx(check_esd)
 
 
-@pytest.fixture(scope="module")
-def cif_with_replacement(tmp_path_factory):
+@pytest.fixture(scope="module", name='cif_with_replacement')
+def fixture_cif_with_replacement(
+    tmp_path_factory: pytest.TempPathFactory
+) -> Tuple[dict, dict, dict]:
+    """
+    Pytest fixture to replace the structure block of one CIF with another.
+
+    This fixture replaces the structure block from the '80K_P_out.cif' CIF file
+    with the structure block from the '105K_P_out.cif' CIF file.
+    """
     from_cif_path = Path('./tests/util/cif_files/80K_P_out.cif')
     to_cif_path = Path('./tests/util/cif_files/105K_P_out.cif')
     from_cif_dataset = '80K_P'
@@ -69,7 +93,11 @@ def cif_with_replacement(tmp_path_factory):
     combined_cif = read_cif_safe(combined_cif_path)
     return from_cif['80K_P'], to_cif['105K_P'], combined_cif['105K_P']
 
-def test_cif_atom_site_copied(cif_with_replacement):
+def test_cif_atom_site_copied(cif_with_replacement: Tuple[dict, dict, dict]):
+    """
+    Test that the atom site block from the original CIF is copied correctly
+    to the combined CIF.
+    """
     from_cif, to_cif, combined_cif = cif_with_replacement
     to_keys = [key for key in to_cif.keys() if key.startswith('_atom_site')]
     from_keys = [key for key in from_cif.keys() if key.startswith('_atom_site')]
@@ -83,17 +111,25 @@ def test_cif_atom_site_copied(cif_with_replacement):
         assert not any('(' in val for val in combined_cif[key])
         from_vals = from_cif[key]
         if any('(' in val for val in from_vals):
-            from_vals, from_esds = split_esds(from_vals)
+            from_vals, _ = split_esds(from_vals)
             combined_vals = np.array([float(val) for val in combined_cif[key]])
             assert np.max(np.abs(from_vals - combined_vals)) < 1e-6
         else:
             for single_from, single_combined in zip(from_vals, combined_cif[key]):
                 assert single_from == single_combined
 
-def test_cif_atom_type_copied(cif_with_replacement):
+def test_cif_atom_type_copied(cif_with_replacement: Tuple[dict, dict, dict]):
+    """
+    Test that the atom type block from the original CIF is copied correctly
+    to the combined CIF.
+    """
     raise NotImplementedError('This is a future feature')
 
-def test_refinement_details_copied(cif_with_replacement):
+def test_refinement_details_copied(cif_with_replacement: Tuple[dict, dict, dict]):
+    """
+    Test that refinement details from the original CIF are copied correctly
+    to the combined CIF.
+    """
     from_cif, _, combined_cif = cif_with_replacement
     test_key = '_shelx_res_file'
     if test_key in combined_cif.keys() or test_key in from_cif.keys():
@@ -103,7 +139,11 @@ def test_refinement_details_copied(cif_with_replacement):
     if test_key in combined_cif.keys() or test_key in from_cif.keys():
         assert combined_cif[test_key] == from_cif[test_key]
 
-def test_space_group_copied(cif_with_replacement):
+def test_space_group_copied(cif_with_replacement: Tuple[dict, dict, dict]):
+    """
+    Test that the space group block from the original CIF is copied correctly
+    to the combined CIF.
+    """
     # TODO add test case where this fails when not implemented properly
     from_cif, to_cif, combined_cif = cif_with_replacement
 
@@ -126,21 +166,37 @@ def test_space_group_copied(cif_with_replacement):
     assert all(from_cif[key] == combined_cif[key] for key in remaining_keys)
 
 
-def test_cif_geom_deleted(cif_with_replacement):
+def test_cif_geom_deleted(cif_with_replacement: Tuple[dict, dict, dict]):
+    """
+    Test that the geometry block (starting with `_geom`) is deleted in the
+    combined CIF.
+    """
     _, _, combined_cif = cif_with_replacement
     assert not any(key.startswith('_geom') for key in combined_cif.keys())
 
-def test_cif_refine_deleted(cif_with_replacement):
+def test_cif_refine_deleted(cif_with_replacement: Tuple[dict, dict, dict]):
+    """
+    Test that the refinement block (starting with `_refine` but not exceptions)
+    is deleted in the combined CIF.
+    """
     _, _, combined_cif = cif_with_replacement
     exceptions = ('_refine_ls_weighting','_refine_ls_extinction')
     assert not any(key.startswith('_refine') and not key.startswith(exceptions)
                    for key in combined_cif.keys())
 
-def test_cif_refln_calc_deleted(cif_with_replacement):
+def test_cif_refln_calc_deleted(cif_with_replacement: Tuple[dict, dict, dict]):
+    """
+    Test that the reflection calculation block (starting with `_refln_` and
+    containing 'calc') is deleted in the combined CIF.
+    """
     _, _, combined_cif = cif_with_replacement
     assert not any(key.startswith('_refln_') and 'calc' in key for key in combined_cif.keys())
 
-def test_cif_cell_kept(cif_with_replacement):
+def test_cif_cell_kept(cif_with_replacement: Tuple[dict, dict, dict]):
+    """
+    Test that the cell block (starting with `_cell`) from the CIF to be
+    merged is kept intact in the combined CIF.
+    """
     from_cif, to_cif, combined_cif = cif_with_replacement
     to_keys = [key for key in to_cif.keys() if key.startswith('_cell')]
     from_keys = [key for key in from_cif.keys() if key.startswith('_cell')]
@@ -152,7 +208,11 @@ def test_cif_cell_kept(cif_with_replacement):
     for key in to_keys:
         assert to_cif[key] == combined_cif[key]
 
-def test_reflns_kept(cif_with_replacement):
+def test_reflns_kept(cif_with_replacement: Tuple[dict, dict, dict]):
+    """
+    Test that the reflections block (starting with `_reflns`) from the CIF
+    to be merged is kept intact in the combined CIF.
+    """
     from_cif, to_cif, combined_cif = cif_with_replacement
     to_keys = [key for key in to_cif.keys() if key.startswith('_reflns')]
     from_keys = [key for key in from_cif.keys() if key.startswith('_reflns')]
@@ -163,7 +223,3 @@ def test_reflns_kept(cif_with_replacement):
 
     for key in to_keys:
         assert to_cif[key] == combined_cif[key]
-
-
-
-
